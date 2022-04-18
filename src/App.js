b69503e96924ce04bbf8221da93b08640bb4c084
nasirdiu <nasir.joypurhat@gmail.com>
2022-04-18 23:09:35 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MyLocation from './components/Location/MyLocation';
+
+function App() {
+  return (
+    <div className="App">
+      <MyLocation></MyLocation>
+    </div>
+  );
+}
+
+export default App;
